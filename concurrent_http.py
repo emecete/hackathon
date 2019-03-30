a279e3beb5521649d183823c73155edca43f06a2
mmp1100000 <marcomunozperez@uma.es>
2019-03-30 19:44:00 +0100

    updates

--- concurrent_http.py
+++ concurrent_http.py
@@ -1,40 +1,34 @@
-import pandas as pd
 import concurrent.futures
-import requests
+import concurrent.futures
 import time
-import asyncio
 
-from instagram_api import get_user_metadata, InstagramApiRequests
+from instagram_api import InstagramApiRequests
 from rankings import hashtag_to_hashtag_id, get_all_hashtags
 
 ir = InstagramApiRequests()
-#out = []
-CONNECTIONS = 100
-TIMEOUT = 5
 
-hashtag_ids = hashtag_to_hashtag_id(get_all_hashtags())
+#hashtag_ids = hashtag_to_hashtag_id(get_all_hashtags())
 
-
-def load_url_get_recent_media(hashtag_id):
-    ans = ir.get_recent_media(hashtag_id)
-    return ans
+#ir.get_recent_media(hashtag_id)
+#ir.get_post_metadata()
 
 
-# with concurrent.futures.ThreadPoolExecutor(max_workers=CONNECTIONS) as executor:
-#     future_to_url = (executor.submit(load_url_get_recent_media, hashtag_id) for hashtag_id in hashtag_ids)
-#     time1 = time.time()
-#     for future in concurrent.futures.as_completed(future_to_url):
-#         try:
-#             data = future.result()
-#         except Exception as exc:
-#             data = str(type(exc))
-#         finally:
-#             #out.append(data)
-#             print("added")
-#             #print(str(len(out)), end="\r")
-#
-#     time2 = time.time()
+def http_threader(function_name, iterable_list, connections=100):
+    out = []
+    with concurrent.futures.ThreadPoolExecutor(max_workers=connections) as executor:
+        future_to_url = (executor.submit(function_name, element) for element in iterable_list)
+        time1 = time.time()
+        for future in concurrent.futures.as_completed(future_to_url):
+            try:
+                data = future.result()
+            except Exception as exc:
+                data = str(type(exc))
+            finally:
+                out.append(data)
 
-# print(f'Took {time2 - time1:.2f} s')
-#print(pd.Series(out).value_counts())
+                print("added")
+                # print(str(len(out)), end="\r")
 
+        time2 = time.time()
+    print(f'Took {time2 - time1:.2f} s')
+    return out
--- instagram_api.py
+++ instagram_api.py
@@ -1,12 +1,24 @@
 import json
-from pprint import pprint
-
 import requests
 
 from utils import get_credentials
 
+HASHTAGS_FILEPATH = 'hashtag.json'
 
-class ApiRequests:
+
+def get_hashtags():
+    with open(HASHTAGS_FILEPATH) as f:
+        return json.load(f)
+
+
+def get_user_metadata(user_id, params='follower_count,media_count,username'):
+    response = requests.get(
+        ' http://hackathon.ocupa2.com/instagram/%s?fields=%s' % (user_id, params))
+    json_data = json.loads(response.text)
+    return json_data
+
+
+class InstagramApiRequests:
     def __init__(self):
         self.token = get_credentials()['token']
 
@@ -14,26 +26,23 @@ class ApiRequests:
         response = requests.get(
             ' http://hackathon.ocupa2.com/instagram/ig_hashtag_search?q=%s&user_id=%s' % (hashtag, self.token))
         json_data = json.loads(response.text)
-<<<<<<< Updated upstream
-        pprint(json_data)
-=======
         return json_data
->>>>>>> Stashed changes
 
     def get_top_media(self, hashtag_id):
         response = requests.get(
             ' http://hackathon.ocupa2.com/instagram/%d/top_media?&user_id=%s' % (hashtag_id, self.token))
         json_data = json.loads(response.text)
-        pprint(json_data)
+        return (json_data)
 
     def get_recent_media(self, hashtag_id):
         response = requests.get(
             ' http://hackathon.ocupa2.com/instagram/%d/recent_media?&user_id=%s' % (hashtag_id, self.token))
         json_data = json.loads(response.text)
-        pprint(json_data)
+        return (json_data)
 
-    def get_post_metadata(self, post_id, id=True, comments_count=True):
+    def get_post_metadata(self, post_id, params='username'):
         response = requests.get(
-            ' http://hackathon.ocupa2.com/instagram/media/%d?fields={fields}' % (post_id, ))
+            ' http://hackathon.ocupa2.com/instagram/media/%d?fields=%s' % (post_id,params))
         json_data = json.loads(response.text)
-        pprint(json_data)
+        return json_data
+
