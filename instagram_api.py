ddb5b01a3da6c21b5a8fda2e722da3add1c8358e
mmp1100000 <marcomunozperez@uma.es>
2019-03-30 13:44:31 +0100

    instagram api implementation (2)

--- instagram_api.py
+++ instagram_api.py
@@ -10,8 +10,26 @@ class ApiRequests:
     def __init__(self):
         self.token = get_credentials()['token']
 
+    def get_hashtag_id(self, hashtag):
+        response = requests.get(
+            ' http://hackathon.ocupa2.com/instagram/ig_hashtag_search?q=%s&user_id=%s' % (hashtag, self.token))
+        json_data = json.loads(response.text)
+        pprint(json_data)
+
     def get_top_media(self, hashtag_id):
         response = requests.get(
             ' http://hackathon.ocupa2.com/instagram/%d/top_media?&user_id=%s' % (hashtag_id, self.token))
+        json_data = json.loads(response.text)
+        pprint(json_data)
+
+    def get_recent_media(self, hashtag_id):
+        response = requests.get(
+            ' http://hackathon.ocupa2.com/instagram/%d/recent_media?&user_id=%s' % (hashtag_id, self.token))
+        json_data = json.loads(response.text)
+        pprint(json_data)
+
+    def get_post_metadata(self, post_id, id=True, comments_count=True):
+        response = requests.get(
+            ' http://hackathon.ocupa2.com/instagram/media/%d?fields={fields}' % (post_id, ))
         json_data = json.loads(response.text)
         pprint(json_data)
