1784457c9c9b0a6b098ea8ebf0b478cfd38f15ee
mmp1100000 <marcomunozperez@uma.es>
2019-03-30 17:01:27 +0100

    implemented post mongo insert

--- instagram_api.py
+++ instagram_api.py
@@ -10,6 +10,14 @@ def get_hashtags():
     with open(HASHTAGS_FILEPATH) as f:
         return json.load(f)
 
+
+def get_user_metadata(user_id, params='follower_count,media_count,username'):
+    response = requests.get(
+        ' http://hackathon.ocupa2.com/instagram/%s?fields=%s' % (user_id, params))
+    json_data = json.loads(response.text)
+    return json_data
+
+
 class InstagramApiRequests:
     def __init__(self):
         self.token = get_credentials()['token']
@@ -32,10 +40,9 @@ class InstagramApiRequests:
         json_data = json.loads(response.text)
         return (json_data)
 
-    def get_post_metadata(self, post_id, params='id, comments_count'):
+    def get_post_metadata(self, post_id, params='username'):
         response = requests.get(
-            ' http://hackathon.ocupa2.com/instagram/media/%d?fields={fields}' % (post_id,params))
+            ' http://hackathon.ocupa2.com/instagram/media/%d?fields=%s' % (post_id,params))
         json_data = json.loads(response.text)
         return json_data
-
 
--- rankings.py
+++ rankings.py
@@ -1,6 +1,7 @@
 from pprint import pprint
 
-from instagram_api import get_hashtags, InstagramApiRequests
+from instagram_api import get_hashtags, InstagramApiRequests, get_user_metadata
+from mongodb.utils import mongo_db
 
 
 def get_all_hashtags():
@@ -20,8 +21,14 @@ def hashtag_to_hashtag_id(hashtag_list):
 
 
 def get_all_posts():
+    mdb = mongo_db()
     ir = InstagramApiRequests()
     for hashtag in get_all_hashtags():
-        pprint(ir.get_recent_media(ir.get_hashtag_id(hashtag)['id']))
+        posts = ir.get_recent_media(ir.get_hashtag_id(hashtag)['id'])['data']
+        for post in posts:
+            user = ir.get_post_metadata(post['id'])[0]['userId']
+            post['user'] = get_user_metadata(user)[0]
+            mdb.add_post(post)
+
 
 get_all_posts()
