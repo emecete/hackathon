b45070415faf761871095d4cb8828aa1fa071c22
Maria <mcttuf0@uma.es>
2019-03-31 12:27:35 +0200

    Updated porsche classifier

---
+++ porsche/train_models.py
@@ -0,0 +1,11 @@
+from hackaton.porsche.conv_porsche import read_data, data_augmentation, train
+import pickle
+
+x, y = read_data('dataset_imagenes_instagram', 'dataset.csv')
+x, y = data_augmentation(x, y)
+
+model, acc = train(x, y)
+
+print(acc)
+filehandler = open("model_3103201901", 'w')
+pickle.dump(model, filehandler)
